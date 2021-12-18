30c531489f038997089de0b0be4fe0bfc4e0f852
JetDeveloping <jetjadeja@gmail.com>
2021-12-17 19:08:52 -0800

    Added exchange tests

---
+++ src/test/CliptoExchange.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.10;
+
+import {CliptoExchange} from "../CliptoExchange.sol";
+import {DSTestPlus} from "lib/solmate/src/test/utils/DSTestPlus.sol";
+
+contract CliptoExchangeTest is DSTestPlus {
+    CliptoExchange exchange;
+
+    function setUp() external {
+        exchange = new CliptoExchange();
+    }
+
+    function testCreatorRegistration() public {
+        // Register creator.
+        exchange.registerCreator("Gabriel Haines", 1e18);
+
+        // Retrieve creator information.
+        (string memory name, uint256 cost) = exchange.creators(address(this));
+
+        // Ensure the data returned is correct.
+        assertEq(name, "Gabriel Haines");
+        assertEq(cost, 1e18);
+    }
+
+    function testRequestCreation() external {
+        // Register a creator.
+        testCreatorRegistration();
+
+        // Create a new request (the creator address is address(this))
+        exchange.newRequest(address(this));
+
+        // Check that the request was created
+        (address requester, uint256 value) = exchange.requests(address(this), 0);
+
+        // Ensure the data returned is correct.
+        assertEq(requester, address(this));
+        assertEq(value, 0);
+    }
+}
