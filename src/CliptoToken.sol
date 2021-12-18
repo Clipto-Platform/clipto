1d457b671108806ad3a36cd49914ecea35395574
Ape Dev <admin@chainstride.capital>
2021-12-18 21:39:34 +0100

    remove erc20

--- src/CliptoToken.sol
+++ src/CliptoToken.sol
@@ -1,10 +1,57 @@
 // SPDX-License-Identifier: None
 pragma solidity 0.8.10;
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CliptoToken is ERC20, Ownable {
-    constructor() ERC20("Clipto", "CTO") {
-        _mint(msg.sender, 1_000_000_000 * 1e18);
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CliptoToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(string memory _creatorName) ERC721(string(abi.encodePacked("Clipto - ", _creatorName)), "CTO") {
+        transferOwnership(tx.origin);
+    }
+
+    // See https://docs.opensea.io/docs/contract-level-metadata
+    function contractURI() public pure returns (string memory) {
+        return "https://clipto.io/contract-metadata.json";
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function safeMint(address to, string memory _tokenURI) public onlyOwner {
+        _safeMint(to, _tokenIdCounter.current());
+        _setTokenURI(_tokenIdCounter.current(), _tokenURI);
+
+        _tokenIdCounter.increment();
+    }
+
+    /*
+     * The following functions are overrides required by Solidity.
+     */
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
