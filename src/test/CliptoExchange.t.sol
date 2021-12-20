5448531055f28ca9d7464db955e22c4af8f23fdf
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-12-21 04:18:58 +0800

    update test for basic correct flow

--- src/test/CliptoExchange.t.sol
+++ src/test/CliptoExchange.t.sol
@@ -15,15 +15,21 @@ contract CliptoExchangeTest is DSTestPlus, IERC721Receiver {
 
     function testCreatorRegistration() public {
         // Register creator.
-        exchange.registerCreator("https://arweave.net/0xprofileurl", 1e18);
+        address tokenAddress = exchange.registerCreator(
+            "Gabriel", 
+            "https://arweave.net/0xprofileurl", 
+            1e18, 
+            1e18
+        );
 
         // Retrieve creator information.
-        (string memory profileUrl, uint256 cost, address token) = exchange.creators(address(this));
+        (string memory profileUrl, uint256 cost, address token, uint minTimeToDeliver) = exchange.creators(address(this));
 
         // Ensure the data returned is correct.
         assertEq(profileUrl, "https://arweave.net/0xprofileurl");
         assertEq(cost, 1e18);
-        assertEq(token, address(0));
+        assertEq(token, tokenAddress);
+        assertEq(minTimeToDeliver, 1e18);
     }
 
     function testRequestCreation() public {
@@ -31,28 +37,28 @@ contract CliptoExchangeTest is DSTestPlus, IERC721Receiver {
         testCreatorRegistration();
 
         // Create a new request (the creator address is address(this))
-        exchange.newRequest{value: 1e18}(address(this));
+        exchange.newRequest{value: 1e18}(address(this), 2e18);
 
         // Check that the request was created
-        (address requester, uint256 value, bool delivered) = exchange.requests(address(this), 0);
+        (address requester, uint256 value, bool delivered, uint256 deadline, bool refunded) = exchange.requests(address(this), 0);
 
         // Ensure the data returned is correct.
         assertEq(requester, address(this));
         assertEq(value, 1e18);
         assertFalse(delivered);
+        assertEq(deadline, 2e18);
+        assertFalse(refunded);
     }
 
     function testRequestDelivery() public {
         testRequestCreation();
 
         uint256 balanceBefore = address(this).balance;
-        exchange.deliverRequest("Gabriel Haines", 0, "http://website.com");
-        (, , bool delivered) = exchange.requests(address(this), 0);
-        (, , address token) = exchange.creators(address(this));
+        exchange.deliverRequest(0, "http://website.com");
+        (, , bool delivered, , ) = exchange.requests(address(this), 0);
 
         assertTrue(delivered);
         assertTrue(address(this).balance > balanceBefore + 9e17);
-        assertEq(CliptoToken(token).name(), "Clipto - Gabriel Haines");
     }
 
     function onERC721Received(
