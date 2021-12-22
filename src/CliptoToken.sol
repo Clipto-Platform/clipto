f1da4cd6b628da007c9c44a0fc3dc1d86e9ab6e9
JetDeveloping <jetjadeja@gmail.com>
2021-12-21 17:15:13 -0800

    Remove inheritance to avoid compiler conflicts

--- src/CliptoToken.sol
+++ src/CliptoToken.sol
@@ -9,47 +9,47 @@ import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CliptoToken is ERC721Upgradeable, ERC721Enumerable, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract CliptoToken is ERC721Upgradeable {
+    // using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    // Counters.Counter private _tokenIdCounter;
 
     function initialize(string memory _creatorName) external {
         __ERC721_init(abi.encodePacked("Clipto - ", _creatorName), "CTO");
     }
 
-    // See https://docs.opensea.io/docs/contract-level-metadata
-    function contractURI() public pure returns (string memory) {
-        return "https://clipto.io/contract-metadata.json";
-    }
+    // // See https://docs.opensea.io/docs/contract-level-metadata
+    // function contractURI() public pure returns (string memory) {
+    //     return "https://clipto.io/contract-metadata.json";
+    // }
 
-    function safeMint(address to, string memory _tokenURI) public onlyOwner {
-        _safeMint(to, _tokenIdCounter.current());
-        _setTokenURI(_tokenIdCounter.current(), _tokenURI);
+    // function safeMint(address to, string memory _tokenURI) public onlyOwner {
+    //     _safeMint(to, _tokenIdCounter.current());
+    //     _setTokenURI(_tokenIdCounter.current(), _tokenURI);
 
-        _tokenIdCounter.increment();
-    }
+    //     _tokenIdCounter.increment();
+    // }
 
-    /*
-     * The following functions are overrides required by Solidity.
-     */
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
+    // /*
+    //  * The following functions are overrides required by Solidity.
+    //  */
+    // function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    //     super._burn(tokenId);
+    // }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
+    // function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    //     return super.tokenURI(tokenId);
+    // }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+    // function _beforeTokenTransfer(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId
+    // ) internal override(ERC721, ERC721Enumerable) {
+    //     super._beforeTokenTransfer(from, to, tokenId);
+    // }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
+    // function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    //     return super.supportsInterface(interfaceId);
+    // }
 }
