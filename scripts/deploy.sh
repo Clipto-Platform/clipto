1e87dfa80a88148e0b8bbb4205428476f9bdb85f
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2022-01-02 01:12:42 +0800

    Comment out deployment address incase of accidents

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,8 @@ set -eo pipefail
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
-FeeDestination="${1:-0x800852637eFA2e5C21C3E82FaD8CcdC708786817}"
+# Please set the fee destination prior to deployment
+# FeeDestination="${1:-0x800852637eFA2e5C21C3E82FaD8CcdC708786817}"
 
 # Deploy.
 TokenAddr=$(deploy CliptoToken)
