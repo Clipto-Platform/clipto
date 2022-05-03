e78e7a053ce0bf3eaff4df37a8cf898cbf05bcf6
ap-atul <atulpatare99@gmail.com>
2022-05-03 09:32:30 +0530

    rebased changes, updated deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,8 @@
 import * as dotenv from "dotenv";
 import { ethers, upgrades } from "hardhat";
+// eslint-disable-next-line node/no-missing-import
+import { CliptoExchange } from "../typechain";
+
 dotenv.config();
 
 async function main() {
@@ -7,18 +10,21 @@ async function main() {
     process.env.FEE_DESTINATION_ADDRESS || "0x7c98C2DEc5038f00A2cbe8b7A64089f9c0b51991";
 
   const CliptoToken = await ethers.getContractFactory("CliptoToken");
+  const cliptoTokenBeacon = await upgrades.deployBeacon(CliptoToken);
+  await cliptoTokenBeacon.deployed();
+
   const CliptoExchange = await ethers.getContractFactory("CliptoExchange");
 
   let cliptoToken = await upgrades.deployBeacon(CliptoToken);
   cliptoToken = await cliptoToken.deployed();
   const cliptoTokenAddress = cliptoToken.address;
 
-  const cliptoExchange = await upgrades.deployProxy(CliptoExchange, [
+  let cliptoExchangeProxy = (await upgrades.deployProxy(CliptoExchange, [
     feeDestination,
-    cliptoTokenAddress,
-  ]);
-  const proxy = await cliptoExchange.deployed();
-  const cliptoExchangeAddress = proxy.address;
+    cliptoTokenBeacon.address,
+  ])) as CliptoExchange;
+  cliptoExchangeProxy = await cliptoExchangeProxy.deployed();
+  const cliptoExchangeAddress = cliptoExchangeProxy.address;
 
   console.log("\n");
   console.log("Owner of the contracts                : ", feeDestination);
